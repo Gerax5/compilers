b440f8838d793aa0fca131956b03e4bccc5af3c1
Czar272 <pablo2003cesar@gmail.com>
2025-11-21 10:33:49 -0600

    chore: clean code

--- program/test/test_mips_generator.py
+++ program/test/test_mips_generator.py
@@ -1,8 +1,8 @@
 import os, sys
-from antlr4 import InputStream, CommonTokenStream, ParseTreeWalker # type: ignore
+from antlr4 import InputStream, CommonTokenStream, ParseTreeWalker  # type: ignore
 
 # Asegura que Python vea los módulos en /program
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from CompiscriptLexer import CompiscriptLexer
 from CompiscriptParser import CompiscriptParser
@@ -13,14 +13,16 @@ from src.utils.Temp import TempManager
 from src.codeGenerator.CodeGenerator import CodeGenerator
 from src.mipsGenerator.MipsGenerator import MIPSGenerator
 
+
 # ---------- helpers ----------
 def parse_src(src: str):
     inp = InputStream(src)
     lex = CompiscriptLexer(inp)
-    ts  = CommonTokenStream(lex)
+    ts = CommonTokenStream(lex)
     parser = CompiscriptParser(ts)
     tree = parser.program()
     return parser, tree
+
 
 def build_symbols(tree):
     errors = Error()
@@ -28,10 +30,12 @@ def build_symbols(tree):
     ParseTreeWalker().walk(stb, tree)
     return stb, errors
 
+
 def type_check(stb, errors, parser, tree):
     tc = TypeChecker(stb.scopes, stb.globalScope, errors, parser)
     tc.visit(tree)
     return tc, errors
+
 
 def gen_code(tree, symbol_table):
     temp_manager = TempManager()
@@ -39,17 +43,19 @@ def gen_code(tree, symbol_table):
     cg.visit(tree)
     return cg
 
+
 def gen_mips(cg, symbol_table):
     mg = MIPSGenerator(cg.quadruples, symbol_table)
     code = mg.generate()
     return code
+
 
 # ---------- tests ----------
 def test_mips_assign_integer():
     src = "const a = 5;"
     parser, tree = parse_src(src)
     stb, errors = build_symbols(tree)
-    tc, errors  = type_check(stb, errors, parser, tree)
+    tc, errors = type_check(stb, errors, parser, tree)
     cg = gen_code(tree, stb.globalScope.symbols)
     code = gen_mips(cg, stb.globalScope.symbols)
 
@@ -58,18 +64,20 @@ def test_mips_assign_integer():
     assert "li $t0, 5" in code
     assert "sw $t0, var_a" in code
 
+
 def test_let_assign_integer():
     src = "let a = 10;"
     parser, tree = parse_src(src)
     stb, errors = build_symbols(tree)
-    tc, errors  = type_check(stb, errors, parser, tree)
-    cg =gen_code(tree, stb.globalScope.symbols)
+    tc, errors = type_check(stb, errors, parser, tree)
+    cg = gen_code(tree, stb.globalScope.symbols)
     code = gen_mips(cg, stb.globalScope.symbols)
 
     assert ".data" in code
     assert "var_a: .word 0" in code
     assert "li $t0, 10" in code
     assert "sw $t0, var_a" in code
+
 
 def test_mips_assign_variable():
     src = """
@@ -78,8 +86,8 @@ def test_mips_assign_variable():
     """
     parser, tree = parse_src(src)
     stb, errors = build_symbols(tree)
-    tc, errors  = type_check(stb, errors, parser, tree)
-    cg =gen_code(tree, stb.globalScope.symbols)
+    tc, errors = type_check(stb, errors, parser, tree)
+    cg = gen_code(tree, stb.globalScope.symbols)
     code = gen_mips(cg, stb.globalScope.symbols)
 
     assert ".data" in code
@@ -90,19 +98,21 @@ def test_mips_assign_variable():
     assert "lw $t0, var_a" in code
     assert "sw $t0, var_b" in code
 
+
 def test_string_literal_handling():
     src = 'print("Hello, World!");'
     parser, tree = parse_src(src)
     stb, errors = build_symbols(tree)
-    tc, errors  = type_check(stb, errors, parser, tree)
-    cg =gen_code(tree, stb.globalScope.symbols)
+    tc, errors = type_check(stb, errors, parser, tree)
+    cg = gen_code(tree, stb.globalScope.symbols)
     code = gen_mips(cg, stb.globalScope.symbols)
 
     assert ".data" in code
     assert 'str0: .asciiz "Hello, World!"' in code
-    assert 'la $a0, str0' in code
-    assert 'li $v0, 4' in code
-    assert 'syscall' in code
+    assert "la $a0, str0" in code
+    assert "li $v0, 4" in code
+    assert "syscall" in code
+
 
 def test_variable_printing():
     src = """
@@ -111,8 +121,8 @@ def test_variable_printing():
     """
     parser, tree = parse_src(src)
     stb, errors = build_symbols(tree)
-    tc, errors  = type_check(stb, errors, parser, tree)
-    cg =gen_code(tree, stb.globalScope.symbols)
+    tc, errors = type_check(stb, errors, parser, tree)
+    cg = gen_code(tree, stb.globalScope.symbols)
     code = gen_mips(cg, stb.globalScope.symbols)
 
     assert ".data" in code
@@ -123,6 +133,7 @@ def test_variable_printing():
     assert "li $v0, 1" in code
     assert "syscall" in code
 
+
 def test_addition_operation():
     src = """
     let a = 10;
@@ -131,8 +142,8 @@ def test_addition_operation():
     """
     parser, tree = parse_src(src)
     stb, errors = build_symbols(tree)
-    tc, errors  = type_check(stb, errors, parser, tree)
-    cg =gen_code(tree, stb.globalScope.symbols)
+    tc, errors = type_check(stb, errors, parser, tree)
+    cg = gen_code(tree, stb.globalScope.symbols)
     code = gen_mips(cg, stb.globalScope.symbols)
 
     assert ".data" in code
@@ -148,6 +159,7 @@ def test_addition_operation():
     assert "add $t2, $t0, $t1" in code
     assert "sw $t0, var_c" in code
 
+
 def test_subtraction_operation():
     src = """
     let a = 30;
@@ -156,8 +168,8 @@ def test_subtraction_operation():
     """
     parser, tree = parse_src(src)
     stb, errors = build_symbols(tree)
-    tc, errors  = type_check(stb, errors, parser, tree)
-    cg =gen_code(tree, stb.globalScope.symbols)
+    tc, errors = type_check(stb, errors, parser, tree)
+    cg = gen_code(tree, stb.globalScope.symbols)
     code = gen_mips(cg, stb.globalScope.symbols)
 
     assert ".data" in code
@@ -173,6 +185,7 @@ def test_subtraction_operation():
     assert "sub $t2, $t0, $t1" in code
     assert "sw $t0, var_c" in code
 
+
 def test_string_concatenation():
     src = """
     let str1 = "Hello, ";
@@ -182,16 +195,17 @@ def test_string_concatenation():
     """
     parser, tree = parse_src(src)
     stb, errors = build_symbols(tree)
-    tc, errors  = type_check(stb, errors, parser, tree)
-    cg =gen_code(tree, stb.globalScope.symbols)
+    tc, errors = type_check(stb, errors, parser, tree)
+    cg = gen_code(tree, stb.globalScope.symbols)
     code = gen_mips(cg, stb.globalScope.symbols)
 
     assert ".data" in code
     assert 'str0: .asciiz "Hello, "' in code
     assert 'str1: .asciiz "World!"' in code
-    assert 'la $a0, str0' in code
-    assert 'li $v0, 4' in code
-    assert 'syscall' in code
+    assert "la $a0, str0" in code
+    assert "li $v0, 4" in code
+    assert "syscall" in code
+
 
 def test_string_int_concatenation():
     src = """
@@ -202,12 +216,12 @@ def test_string_int_concatenation():
     """
     parser, tree = parse_src(src)
     stb, errors = build_symbols(tree)
-    tc, errors  = type_check(stb, errors, parser, tree)
-    cg =gen_code(tree, stb.globalScope.symbols)
+    tc, errors = type_check(stb, errors, parser, tree)
+    cg = gen_code(tree, stb.globalScope.symbols)
     code = gen_mips(cg, stb.globalScope.symbols)
 
     assert ".data" in code
     assert 'str0: .asciiz "The answer is: "' in code
-    assert 'la $a0, str0' in code
-    assert 'li $v0, 4' in code
-    assert 'syscall' in code
+    assert "la $a0, str0" in code
+    assert "li $v0, 4" in code
+    assert "syscall" in code
